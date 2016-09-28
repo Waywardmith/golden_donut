fb50c368e5f33c6a726706935ca1a5b819580945
Simone <siritheill@gmail.com>
2016-09-28 09:55:28 +0200

    Added solution

---
+++ golden_donut/golden_donut/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace golden_donut
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
