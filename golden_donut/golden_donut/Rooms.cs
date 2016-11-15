124751fc611a543a9b661ce2671beedc25a554e1
nicolinekruse <nicokrusefrank@hotmail.com>
2016-11-15 12:57:40 +0100

    updated rooms

--- golden_donut/golden_donut/Rooms.cs
+++ golden_donut/golden_donut/Rooms.cs
@@ -21,7 +21,31 @@ namespace golden_donut
             Room r5 = new Room("Toulouse");
             Room r6 = new Room("Nice");
             Room r7 = new Room("Marseille");
-            Room r8 = new Room("Paris");
+			Room r8 = new Room("Lyon");
+			Room r9 = new Room("Nates");
+			Room r10 = new Room("Rennes");
+			Room r11 = new Room("Le Havre");
+			Room r12 = new Room("Reims");
+			Room r13 = new Room("Saint-Etienne");
+			Room r14 = new Room("Toulon");
+			Room r15 = new Room("Grenoble");
+            Room r16 = new Room("Paris");
+
+			Room r17 = new Room("The Louvre");
+			Room r18 = new Room("The Créme Bûléé Crater");
+			Room r19 = new Room("The Musée d'Orsay");
+			Room r20 = new Room("The Moulin Rouge");
+			Room r21 = new Room("The Champs-Élysées");
+			Room r22 = new Room("The Notre Dame de Paris");
+			Room r23 = new Room("The Basilica of Sacre Coeur");
+			Room r24 = new Room("Arc de Triomphe");
+			Room r25 = new Room("Montmartre");
+			Room r26 = new Room("Sainte-Chapelle");
+			Room r27 = new Room("Latin Quarter");
+			Room r28 = new Room("Pére Lachaise Cemetery");
+			Room r29 = new Room("Panthéon");
+
+			Room r30 = new Room("The Eiffel Tower");
 
             rooms.Add(r1);
             rooms.Add(r2);
